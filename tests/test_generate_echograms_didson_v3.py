816661f0cef80af61574de58d23fc32c6c13c0fb
MAHobley <mahobley@robots.ox.ac.uk>
2025-02-12 17:18:23 -0800

    made didson version 3 unit test file

---
+++ tests/test_generate_echograms_didson_v3.py
@@ -0,0 +1,39 @@
+import pytest
+
+from conftest import DDF_FILE
+
+from fisheye.config import ARISDatasetConfig
+from utils.visualisation_utils import generate_echogram_gif_from_aris
+
+"""
+Same as test_generate_echograms but using didson version 3 file type"""
+
+
+def test_generate_echogram_gif_from_small_aris():
+    config = ARISDatasetConfig(
+        aris_filepath=DDF_FILE,
+        return_unwarped=False,
+        return_echogram=True,
+    )
+
+    generate_echogram_gif_from_aris(
+        config,
+        save_filename="",
+        echogram_pop=True,
+        return_unwarped=False,
+    )
+
+
+def test_generate_echogram_gif_from_small_aris_no_echo_pop():
+    config = ARISDatasetConfig(
+        aris_filepath=DDF_FILE,
+        return_unwarped=False,
+        return_echogram=True,
+    )
+
+    generate_echogram_gif_from_aris(
+        config,
+        save_filename="",
+        echogram_pop=False,
+        return_unwarped=False,
+    )
