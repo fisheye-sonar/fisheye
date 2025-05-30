8e1e059f9e335359185784e683003f28a0bd5c25
Madison Van Horn <67763032+madivanhorn@users.noreply.github.com>
2025-05-30 15:11:51 -0700

    Comments from beta testing

--- run.sh
+++ run.sh
@@ -3,26 +3,28 @@
 ### MAKE CHANGES HERE ###
 
 # Path to the folder containing your ARIS or DIDSON files
-DATA_DIR=${1:-"./data"}
+INPUT_DIR=${1:-""}
 
 # Path to your trained model weights file
-MODEL_PATH=${2:-"./models/model.pt"}
+MODEL_PATH=${2:-""}
 
 # What to export. Options include: summary_csv, detailed_csv, txt, mot, none. Use "none" to skip exporting.
-EXPORT=${3:-"summary_csv,detailed_csv,txt"}
+EXPORT_OPTIONS=${3:-"summary_csv,detailed_csv,txt"}
 
 # Where to save the output files.
 # You can choose any folder. If you save to the same folder as your ARIS/DIDSON files, ARISFish Software will be able to
 # read the model outputs and display results in its interface. Otherwise, you'll need to manually copy the .txt output
 # files to the same folder later if you want to view them in the ARISFish interface.
-OUTPUT_DIR=${4:-"./data/"}
+# If empty or not provided, defaults to same as INPUT_DIR
+RAW_OUTPUT_DIR=${4:-""}
+OUTPUT_DIR=${RAW_OUTPUT_DIR:-$INPUT_DIR}
 
 ### END OF CHANGES ###
 
 
 ### DO NOT CHANGE ANYTHING BELOW THIS LINE ###
 poetry run python fisheye/main.py \
-  --path "$DATA_DIR" \
+  --path "$INPUT_DIR" \
   --weights "$MODEL_PATH" \
-  --export_options "$EXPORT" \
+  --export_options "$EXPORT_OPTIONS" \
   --output_dir "$OUTPUT_DIR"
