Fix DIDSON distance/unwarping calculation errors (#276)
* Calculate beam width data for DIDSON files

* Use protocol from Echoview for calculating windowstart and windowlength

* Update comment

fisheye/dataloaders/didson/pyDIDSON.py
@@ -9,14 +9,15 @@ Portions of this code were adapted from SoundMetrics MATLAB code.
 __version__ = "b1.0.2"
 
 import contextlib
+import os
+import struct
 import warnings
+from pathlib import Path
+from types import SimpleNamespace
 from typing import Union
 
 import numpy as np
-import os
-import struct
-from types import SimpleNamespace
-from pathlib import Path
+import pandas as pd
 
 from . import pyARIS
 from .pyDIDSON_format import *
@@ -178,26 +179,56 @@ class DIDSON:
             raise NotImplementedError
 
         elif version_id == 3:
-            # Convert windowlength code to meters
-            info["windowlength"] = {
-                0b00: [0.83, 2.5, 5, 10, 20, 40],  # DIDSON-S, Extended Windows
-                0b01: [1.125, 2.25, 4.5, 9, 18, 36],  # DIDSON-S, Classic Windows
-                0b10: [2.5, 5, 10, 20, 40, 70],  # DIDSON-LR, Extended Window
-                0b11: [2.25, 4.5, 9, 18, 36, 72],  # DIDSON-LR, Classic Windows
-            }[info["configflags"] & 0b11][
-                info["windowlength"] + 2 * (1 - info["resolution"])
-            ]
-
-            # Windowstart 1 to 31 times 0.75 (Lo) or 0.375 (Hi) or 0.419 for extended
-            info["windowstart"] = {
-                0b0: 0.419
-                * info["windowstart"]
-                * (2 - info["resolution"]),  # meters for extended DIDSON
-                0b1: 0.375 * info["windowstart"] * (2 - info["resolution"]),
-                # meters for standard or long range DIDSON
-            }[info["configflags"] & 0b1]
-
             info["halffov"] = 14.4
+            info["BeamCount"] = info["numbeams"]
+
+            total_fov = 2 * info["halffov"]
+            beam_width = total_fov / info["numbeams"]
+
+            centers = np.linspace(
+                -info["halffov"] + beam_width / 2,
+                info["halffov"] - beam_width / 2,
+                info["numbeams"],
+            )
+
+            left = centers - beam_width / 2
+            right = centers + beam_width / 2
+
+            beam_width_data = pd.DataFrame(
+                {
+                    "beam_num": np.arange(info["numbeams"]),
+                    "beam_center": centers,
+                    "beam_left": left,
+                    "beam_right": right,
+                }
+            )
+
+            # The following protocol is from
+            # https://support.echoview.com/WebHelp/Reference/File_Formats/DIDSON_data_files.htm for DDF_03
+            soundspeed = 1500  # Defaulted to the DIDSON specified sound speed of 1500/s
+
+            is_high_res = info["resolution"] == 1
+            is_serial_num_gt_18 = info["serialnumber"] > 18
+            if is_high_res:
+                delay_period = 0.000572 if is_serial_num_gt_18 else 0.000512
+            else:
+                delay_period = 0.001144 if is_serial_num_gt_18 else 0.001024
+
+            info["windowstart"] = info["windowstart"] * delay_period * soundspeed / 2.0
+            info["windowlength"] = (
+                info["samplesperchannel"] * soundspeed / (2.0 * info["samplerate"])
+            )
+
+            sampleperiod = (1.0 / info["samplerate"]) * 1e6
+            info.update(
+                {
+                    "beam_width_dir": os.path.abspath(beam_width_dir),
+                    "beam_width_data": beam_width_data,
+                    "sampleperiod": sampleperiod,
+                    "soundspeed": soundspeed,
+                    "samplesperbeam": info["samplesperchannel"],
+                }
+            )
 
         elif version_id == 4:
             # Convert windowlength code to meters
@@ -209,6 +240,7 @@ class DIDSON:
             info["windowstart"] = 0.419 * info["windowstart"] * (2 - info["resolution"])
 
             info["halffov"] = 14.4
+
         elif version_id == 5:  # ARIS
             if info["pingmode"] in [1, 2]:
                 BeamCount = 48
@@ -315,20 +347,18 @@ class DIDSON:
             info["xdim"] = 300 if ixsize == -1 else ixsize
             ydim, xdim, write_rows, write_cols, read_i = DIDSON.mapscan(info)
 
-            # widthscale meters/pixels
-            pixel_meter_width = (
-                2
-                * (info["windowstart"] + info["windowlength"])
-                * np.sin(np.radians(14.25))
-                / xdim
-            )
-            # heightscale meters/pixels
-            pixel_meter_height = (
-                (info["windowstart"] + info["windowlength"])
-                - info["windowstart"] * np.cos(np.radians(14.25))
-            ) / ydim
+            rmin = info["windowstart"]
+            rmax = rmin + info["windowlength"]
+            halffov_rad = np.radians(info["halffov"])
 
-            pixel_meter_size = (pixel_meter_width + pixel_meter_height) / 2
+            pixel_meter_size = (2 * rmax * np.sin(halffov_rad)) / xdim
+            pixel_meter_width = pixel_meter_size
+            pixel_meter_height = pixel_meter_size
+
+            x_meter_start = -rmax * np.sin(halffov_rad)
+            x_meter_stop = rmax * np.sin(halffov_rad)
+            y_meter_start = rmax
+            y_meter_stop = rmin * np.cos(halffov_rad)
 
         unwarped_shape = [
             info["samplesperchannel"],
@@ -346,6 +376,10 @@ class DIDSON:
                 "pixel_meter_width": pixel_meter_width,
                 "pixel_meter_height": pixel_meter_height,
                 "pixel_meter_size": pixel_meter_size,
+                "x_meter_start": x_meter_start,
+                "x_meter_stop": x_meter_stop,
+                "y_meter_start": y_meter_start,
+                "y_meter_stop": y_meter_stop,
                 "unwarped_shape": unwarped_shape,
             }
         )

fisheye/utils.py
@@ -185,9 +185,11 @@ def get_unwarped_distance_and_theta(row: pd.Series):
             - theta: Beam center angle in degrees
     """
     metadata = row["metadata"]
-    metadata.beam_width_data, _ = pyARIS.load_beam_width_data(
-        frame=metadata, beam_width_dir=BEAM_WIDTH_DIR
-    )
+
+    if metadata.beam_width_data is None:
+        metadata.beam_width_data, _ = pyARIS.load_beam_width_data(
+            frame=metadata, beam_width_dir=BEAM_WIDTH_DIR
+        )
 
     bbox_xywh = np.array(row["bbox"]) * np.array(
         [metadata.xdim, metadata.ydim, metadata.xdim, metadata.ydim]